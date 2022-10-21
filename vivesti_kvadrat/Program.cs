b823d2dc4e1408f049897dd104aabd42a41eeae4
Konstantin <legenzov-k@ya.ru>
2022-10-21 15:31:27 +0300

    Приложение по выведению квадрата

---
+++ vivesti_kvadrat/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задачка под нахождение квадрата числа
+
+int a, b;
+
+string UserEnter;
+
+Console.Write("Введите число ");
+UserEnter = Console.ReadLine()!;
+
+a = int.Parse(UserEnter);
+b = a * a;
+
+Console.Write($"Квадрат введенного числа = {b}");
